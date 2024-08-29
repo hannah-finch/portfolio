6d62563734e3f11a279863741304bded3e7a1b0b
hannahschwen <hannahschwen@gmail.com>
2024-08-29 01:05:25 -0400

    structure and style header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,25 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 // Will use Link components from react-router-dom to conditionally render the different sections
 // Click a link = highlight active link, url changes, corresponding section displays
 
 function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="shadow1">
+      <h1><span className="teal">Hannah Belle</span> : <span className="green">Full-Stack Developer</span></h1>
       <nav>
-        <Link to={'/about'}>About Me</Link>
-        <Link to={'/portfolio'}>Portfolio</Link>
-        <Link to={'/contact'}>Contact</Link>
-        <Link to={'/resume'}>Resume</Link>
+        <NavLink to={'/about'}className={({ isActive }) => (isActive ? "active-link" : "normal-link")}>
+          .about()
+        </NavLink>
+        <NavLink to={'/portfolio'}className={({ isActive }) => (isActive ? "active-link" : "normal-link")}>
+          .portfolio()
+        </NavLink>
+        <NavLink to={'/contact'}className={({ isActive }) => (isActive ? "active-link" : "normal-link")}>
+          .contact()
+        </NavLink>
+        <NavLink to={'/resume'}className={({ isActive }) => (isActive ? "active-link" : "normal-link")}>
+          .resume()
+        </NavLink>
       </nav>
     </header>
   )
