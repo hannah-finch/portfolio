ae8741f9405a0f04376e4fb1296b4fb6a0228876
hannahschwen <hannahschwen@gmail.com>
2024-08-31 01:59:13 -0400

    add link to project-card images

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -12,23 +12,25 @@ function ProjectCard() {
     
       {projectData.projects.map(project => (
         
-        <div key={project.id} className="card shadow1">
+        <section key={project.id} className="card shadow1">
 
           <div>
-            <div
-              className="img2 shadow3"
-              style={{ backgroundImage: `url(${project.image2})` }}>
+            <Link to={`/project/${project.id}`}>
               <div
-                className="img1"
-                style={{ backgroundImage: `url(${project.image1})` }}>
+                className="img2 shadow3"
+                style={{ backgroundImage: `url(${project.image2})` }}>
+                <div
+                  className="img1"
+                  style={{ backgroundImage: `url(${project.image1})` }}>
+                </div>
               </div>
-            </div>
+            </Link>
             <h3>{project.name}</h3>
             <p>{project.shortDescription}</p>
           </div>
 
           <Link to={`/project/${project.id}`} className="details-btn">Details --></Link>
-        </div>
+        </section>
 
         
       ))}
