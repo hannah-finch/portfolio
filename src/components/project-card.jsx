d94ff4e4c7ddab1314db217b4d5a7e77390d3b67
hannahschwen <hannahschwen@gmail.com>
2024-08-31 01:52:47 -0400

    add hover effect to project-card images

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -1,8 +1,11 @@
 import projectData from '../../public/project-data.json'
 import '../assets/css/project-card.css'
+
 import { Link } from 'react-router-dom';
 
 function ProjectCard() {
+
+ 
   // map through projects in data to render info on cards
   return (
     <>
@@ -10,9 +13,15 @@ function ProjectCard() {
       {projectData.projects.map(project => (
         
         <div key={project.id} className="card shadow1">
-          
+
           <div>
-            <div className="img-box shadow3" style={{ backgroundImage: `url(${project.image1})` }}>
+            <div
+              className="img2 shadow3"
+              style={{ backgroundImage: `url(${project.image2})` }}>
+              <div
+                className="img1"
+                style={{ backgroundImage: `url(${project.image1})` }}>
+              </div>
             </div>
             <h3>{project.name}</h3>
             <p>{project.shortDescription}</p>
@@ -21,25 +30,10 @@ function ProjectCard() {
           <Link to={`/project/${project.id}`} className="details-btn">Details --></Link>
         </div>
 
-
-
-
-
-        // <Link to={`/project/${project.id}`} key={project.id} className="card">
-          
-
-        //   <section className="">
-        //     <img src={project.image1}></img>
-        //     <h3>{project.name}</h3>
-        //     <p>{project.shortDescription}</p>
-
-        //     <Link className="btn-red">.more()</Link>
-        //   </section>
-        //   <div className="inspect"></div>
-        
-        // </Link>
         
       ))}
+
+
     </>
   )
 };
