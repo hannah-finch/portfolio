f5066d8c17ebb64c185a45b5995f3d6a8021f1a0
hannahschwen <hannahschwen@gmail.com>
2024-09-04 20:42:33 -0400

    restructure project page
    Pulled components out of the return, added conditionals to project data so information only displays if it's included in the JSON.

--- src/pages/project-page.jsx
+++ src/pages/project-page.jsx
@@ -5,37 +5,124 @@ import { useParams, Link } from 'react-router-dom';
 import '../assets/css/project-page.css'
 import projectData from '../assets/project-data.json'
 
-// Project Page, render information for a single project
+
+const Details = ({ startDate, endDate, objective, contribution, technologies }) => {
+  
+  const Date = (() => {
+    if (startDate && endDate) {
+      return <p><span className="h2 green">Date: </span>{startDate} - {endDate}</p>
+    }
+  })
+
+  const Objective = (() => {
+    if (objective) {
+      return <p><span className="h2 green">Objective: <br></br></span>{objective}</p>
+    }
+  })
+
+  const Technologies = (() => {
+    if (technologies) {
+      const list = (technologies.map((tech, index) => {
+        return <li key={index} className="tech">{tech}</li>
+      }))
+
+      return <p><span className="h2 green">Technologies: <br></br></span>{list}</p>
+    }
+  })
+
+  const Contribution = (() => {
+    if (contribution) {
+      return <p><span className="h2 green">My Contribution: <br></br></span>{contribution}</p>
+    }
+  })
+
+  return (
+    <div>
+      <h3>Details:</h3>
+      < Date />
+      < Objective />
+      < Technologies />
+      < Contribution />
+    </div>
+  )
+}
+
+const Description = ({ name, longDescription }) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{longDescription}</p>
+    </div>
+  )
+}
+
+const Buttons = ({ deployed, video, github }) => {
+
+  const DeployBtn = (() => {
+    if (deployed) {
+      return <a className="github-link" href={deployed} target="_blank">
+      Deployed Application
+      </a>
+    }
+  })
+
+  const VideoBtn = (() => {
+    if (video) {
+      return <a className="github-link" href={video} target="_blank">
+      Video Walkthrough
+      </a>
+    }
+  })
+
+  const GithubBtn = (() => {
+    if (github) {
+      return <a className="github-link" href={github} target="_blank">
+      View Code on GitHub
+      </a>
+    }
+  })
+
+  return (
+    <div className="btn-container2">
+      < DeployBtn />
+      < VideoBtn />
+      < GithubBtn />
+    </div>
+  )
+}
+
+const ImageSection = ({ image1, moreImages }) => {
+
+  const Image1 = (() => {
+    if (image1) {
+      return <img className="shadow1 main-img" src={image1}></img>
+    }
+  })
+
+  const Images = (() => {
+    if (moreImages) {
+      const images = (moreImages.map((image, index) => {
+        return <div key={index} className="grid-img" style={{ backgroundImage: `url(${image})` }}></div>
+      }))
+      return(images)
+    }
+  })
+
+  return (
+    <>
+      <h3>Screenshots:</h3>
+        < Image1 />
+      <div className="grid">
+        < Images />
+      </div>
+    </>
+  )
+}
 
 function ProjectPage() {
   // get project id from url
   const { id } = useParams();
-
-  // get project data by index = id
-  // this is a temporary solution and should be replaced with a method that doesn't require the project id to equal its index in the projectData array
   const project = projectData.projects[id];
-
-  const getTech = (
-    project.technologies.map((tech, index) => {
-      return ( 
-        <li key={index} className="tech">{tech}</li>
-      )
-    })
-  )
-
-  const getImages = (
-    project.moreImages.map(image => {
-      if (!image) {
-        return
-      } else {
-        return ( 
-          <>
-            <div className="grid-img" style={{ backgroundImage: `url(${image})` }}></div>
-          </>
-        )
-      }
-    })
-  )
 
   return (
     <>
@@ -43,43 +130,21 @@ function ProjectPage() {
 
       <section className="delay">
 
-      <section className="text-box shadow1">
-        <div className="info-container">
-          <div>
-            <h1>{project.name}</h1>
-            <p>{project.longDescription}</p>
+        <section className="text-box shadow1">
+          <div className="info-container">
+            <Description {...project}/>
+            <Details {...project} />
           </div>
-          <div>
-            <h3>Details:</h3>
-            <p><span className="h2 green">Date: </span>{project.startDate} - {project.endDate}</p>
-            {/* <p><span className="h2 green">Objective: <br></br></span>{project.objective}</p> */}
-            <p><span className="h2 green">Technologies: <br></br></span>{getTech}</p>
-            {/* <p><span className="h2 green">My Contribution: <br></br></span>{project.contribution}</p> */}
-          </div>
-        </div>
+          <Buttons {...project} />
+        </section>
 
-        <div className="btn-container2">
-          <a className="github-link" href={project.deployed} target="_blank">
-          Deployed Application
-          </a>
-          <a className="github-link" href={project.github} target="_blank">
-          View Code on GitHub --&gt;
-          </a>
-        </div>
-        
+        <div className="spacer"></div>
+
+        <ImageSection {...project} />
+
+        <Link to="/portfolio">&lt;-- Back to portfolio</Link>
+
       </section>
-
-      <h3>Screenshots:</h3>
-
-      <img className="shadow1 main-img" src={project.image1}></img>
-
-      <div className="grid">
-        {getImages}
-      </div>
-     
-      </section>
-
-      <Link to="/portfolio">&lt;-- Back to portfolio</Link>
     </>
   )
 }
